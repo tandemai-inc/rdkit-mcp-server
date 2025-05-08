d9e441f92a2442add68c94dd73397ae0daf33722
Mike Rosengrant <michael.rosengrant@tandemai.com>
2025-05-08 13:14:14 -0400

    remove mcp_client

--- run_client.py
+++ run_client.py
@@ -1,18 +1,6 @@
 import asyncio
-from src.clients.mcp_client import main as mcp_main
-from src.clients.openai import main as openai_main
+from src.clients.openai import main as openai_client_main
 
 
 if __name__=="__main__":
-    OPENAI = 'openai'
-    MCP = 'mcp'
-
-    # TODO: Set this with command line args
-    client_to_use = OPENAI
-
-    if client_to_use == OPENAI:
-        main_fn = openai_main
-    else:
-        main_fn = mcp_main
-
-    asyncio.run(main_fn())
+    asyncio.run(openai_client_main())
