465eebe70cabb75282e85f3c232bcb88ea7da518
Mike Rosengrant <michael.rosengrant@tandemai.com>
2025-05-16 12:48:08 -0400

    refactor tools to use mcp.tool decorator, but not import instantiated object

--- src/rdkit_mcp_server/server.py
+++ src/rdkit_mcp_server/server.py
@@ -1,34 +1,14 @@
-import itertools
 import os
 import logging
 from mcp.server.fastmcp import FastMCP
 
+from .tools import register_tools
+
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 
+
 mcp = FastMCP("RDKit-MCP Server")
-
-from . import tools as base_tools
-from .rdkit.Chem import Descriptors as descriptor_tools
-
-# Modules to search for tools
-TOOL_MODULES = [
-    base_tools,
-    descriptor_tools,
-]
-
-def get_all_tools(tool_modules):
-    for module in tool_modules:
-        tool_iter = (
-            getattr(module, func)
-            for func in dir(module)
-            if callable(getattr(module, func))
-            # Filter out private functions and those not starting with "_"
-            and not func.startswith("_")
-            # Only include functions defined in the module
-            and getattr(getattr(module, func), "__module__", None) == module.__name__
-        )
-        yield from tool_iter
 
 
 def main():
@@ -45,15 +25,7 @@ def main():
         port = 8000
     
     logger.info("Registering tools with MCP server...")
-    for tool in get_all_tools(TOOL_MODULES):
-        try:
-            mcp.add_tool(
-                tool,
-                name=tool.__name__,
-                description=tool.__doc__,
-            )
-        except Exception as e:
-            logger.error(f"Failed to register tool {tool.__name__}: {e}")
+    register_tools(mcp)
 
     logger.info(f"Starting RDKit MCP Server with transport: {transport}")
     if transport == "sse":
---
+++ src/rdkit_mcp_server/tools/register_tools.py
@@ -0,0 +1,23 @@
+from  .tools import register_tools as base_tools
+from .rdkit.Chem.Descriptors import register_tools as rdkit_chem_descriptor_tools
+from mcp.server.fastmcp import FastMCP
+
+
+__all__ = ["register_tools"]
+
+# Modules to search for tools
+tool_registry_fns = [
+    base_tools,
+    rdkit_chem_descriptor_tools,
+]
+
+def register_tools(mcp: FastMCP) -> None:
+    """
+    Register tools with the MCP server.
+
+    Parameters:
+    - mcp (FastMCP): The MCP server instance.
+    """
+    for tool_register_fn in tool_registry_fns:
+        # Register tools from the specified modules
+        tool_register_fn(mcp)
