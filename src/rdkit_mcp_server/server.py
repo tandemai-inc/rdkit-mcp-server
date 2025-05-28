7af0e0bbcd8ded296a820f5a6fe3fe56b1a78886
Mike Rosengrant <michael.rosengrant@tandemai.com>
2025-05-28 16:04:21 -0400

    clean up main()

--- src/rdkit_mcp_server/server.py
+++ src/rdkit_mcp_server/server.py
@@ -19,24 +19,20 @@ mcp = FastMCP("RDKit-MCP Server")
 async def main():
     """Main function to run the MCP server."""
     args, _ = parser.parse_known_args()
-
-    logger.info(f"Using transport: {args.transport}")
-    host = args.host
-    port = args.port
     transport = args.transport
 
     # TODO: Add settings to have whitelist and blacklist
-    logger.info("Registering tools with MCP server...")
     whitelist = []
     blacklist = []
+    logger.info("Registering tools with MCP server...")
     await register_tools(mcp, whitelist=whitelist, blacklist=blacklist)
 
     logger.info(f"Starting RDKit MCP Server with transport: {transport}")
     if transport == "sse":
-        logger.info(f"Server will run on {host}:{port} using SSE transport.")
-        mcp.settings.host = host
-        mcp.settings.port = port
+        logger.info(f"Server running on {args.host}:{args.port} using SSE transport.")
+        mcp.settings.host = args.host
+        mcp.settings.port = args.port
         await mcp.run_sse_async()
     elif transport == "stdio":
-        logger.info("Server will run using stdio transport.")
+        logger.info("Server running using stdio transport.")
         await mcp.run_stdio_async()
