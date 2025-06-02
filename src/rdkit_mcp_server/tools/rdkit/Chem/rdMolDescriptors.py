706d170f7908a4c16a45e5b6c9b18fc2ca4c8483
Mike Rosengrant <michael.rosengrant@tandemai.com>
2025-06-02 18:25:17 -0400

    Add rdMolDescriptors, enable calc_num_rotatable_bonds

---
+++ src/rdkit_mcp_server/tools/rdkit/Chem/rdMolDescriptors.py
@@ -0,0 +1,180 @@
+from typing import Annotated
+from pydantic import Field
+from rdkit import Chem
+
+from rdkit.Chem.rdMolDescriptors import (
+    CalcExactMolWt,
+    CalcMolFormula,
+    CalcNumRings,
+    CalcNumAromaticRings,
+    CalcNumAliphaticRings,
+    CalcNumRotatableBonds,
+    CalcTPSA,
+    CalcChi0v,
+    CalcChi1v,
+    CalcChi2v,
+    CalcChi3v,
+    CalcChi4v,
+    CalcKappa1,
+    CalcKappa2,
+    CalcKappa3,
+    CalcLabuteASA,
+    CalcCrippenDescriptors,
+    CalcNumHBA,
+    CalcNumHBD,
+    CalcNumLipinskiHBA,
+    CalcNumLipinskiHBD,
+    CalcNumAmideBonds,
+    CalcFractionCSP3,
+    CalcPBF,
+    GetUSRScore,
+    GetUSR,
+)
+
+from ...utils import rdkit_tool
+
+
+@rdkit_tool(enabled=False)
+def calc_exact_mol_wt(*args, **kwargs):
+    return CalcExactMolWt(*args, **kwargs)
+
+
+@rdkit_tool(enabled=False)
+def calc_mol_formula(*args, **kwargs):
+    return CalcMolFormula(*args, **kwargs)
+
+
+@rdkit_tool(enabled=False)
+def calc_num_rings(*args, **kwargs):
+    return CalcNumRings(*args, **kwargs)
+
+
+@rdkit_tool(enabled=False)
+def calc_num_aromatic_rings(*args, **kwargs):
+    return CalcNumAromaticRings(*args, **kwargs)
+
+
+@rdkit_tool(enabled=False)
+def calc_num_aliphatic_rings(*args, **kwargs):
+    return CalcNumAliphaticRings(*args, **kwargs)
+
+
+@rdkit_tool(enabled=True)
+def calc_num_rotatable_bonds(
+        smiles: Annotated[str, Field(description="SMILES representation of a molecule")],
+        strict: Annotated[bool, Field(description="Handles linkages between ring systems.")]=True) -> int:
+    """Calculate the number of rotatable bonds in a molecule given its SMILES representation.
+    
+    Args:
+        smiles (str): The SMILES representation of the molecule.
+        strict (bool): Handles linkages between ring systems.
+            Single bonds between aliphatic ring Cs are always rotatable.
+            This means that the central bond in CC1CCCC(C)C1-C1C(C)CCCC1C is now considered
+            rotatable; it was not before
+            
+            Heteroatoms in the linked rings no longer affect whether or not
+            the linking bond is rotatable the linking bond in systems like
+            Cc1cccc(C)c1-c1c(C)cccc1 is now considered non-rotatable
+    """
+    mol = Chem.MolFromSmiles(smiles)
+    return CalcNumRotatableBonds(mol, strict)
+
+
+@rdkit_tool(enabled=False)
+def calc_tpsa(*args, **kwargs):
+    return CalcTPSA(*args, **kwargs)
+
+
+@rdkit_tool(enabled=False)
+def calc_chi0v(*args, **kwargs):
+    return CalcChi0v(*args, **kwargs)
+
+
+@rdkit_tool(enabled=False)
+def calc_chi1v(*args, **kwargs):
+    return CalcChi1v(*args, **kwargs)
+
+
+@rdkit_tool(enabled=False)
+def calc_chi2v(*args, **kwargs):
+    return CalcChi2v(*args, **kwargs)
+
+
+@rdkit_tool(enabled=False)
+def calc_chi3v(*args, **kwargs):
+    return CalcChi3v(*args, **kwargs)
+
+
+@rdkit_tool(enabled=False)
+def calc_chi4v(*args, **kwargs):
+    return CalcChi4v(*args, **kwargs)
+
+
+@rdkit_tool(enabled=False)
+def calc_kappa1(*args, **kwargs):
+    return CalcKappa1(*args, **kwargs)
+
+
+@rdkit_tool(enabled=False)
+def calc_kappa2(*args, **kwargs):
+    return CalcKappa2(*args, **kwargs)
+
+
+@rdkit_tool(enabled=False)
+def calc_kappa3(*args, **kwargs):
+    return CalcKappa3(*args, **kwargs)
+
+
+@rdkit_tool(enabled=False)
+def calc_labute_asa(*args, **kwargs):
+    return CalcLabuteASA(*args, **kwargs)
+
+
+@rdkit_tool(enabled=False)
+def calc_crippen_descriptors(*args, **kwargs):
+    return CalcCrippenDescriptors(*args, **kwargs)
+
+
+@rdkit_tool(enabled=False)
+def calc_num_hba(*args, **kwargs):
+    return CalcNumHBA(*args, **kwargs)
+
+
+@rdkit_tool(enabled=False)
+def calc_num_hbd(*args, **kwargs):
+    return CalcNumHBD(*args, **kwargs)
+
+
+@rdkit_tool(enabled=False)
+def calc_num_lipinski_hba(*args, **kwargs):
+    return CalcNumLipinskiHBA(*args, **kwargs)
+
+
+@rdkit_tool(enabled=False)
+def calc_num_lipinski_hbd(*args, **kwargs):
+    return CalcNumLipinskiHBD(*args, **kwargs)
+
+
+@rdkit_tool(enabled=False)
+def calc_num_amide_bonds(*args, **kwargs):
+    return CalcNumAmideBonds(*args, **kwargs)
+
+
+@rdkit_tool(enabled=False)
+def calc_fraction_csp3(*args, **kwargs):
+    return CalcFractionCSP3(*args, **kwargs)
+
+
+@rdkit_tool(enabled=False)
+def calc_pbf(*args, **kwargs):
+    return CalcPBF(*args, **kwargs)
+
+
+@rdkit_tool(enabled=False)
+def get_usr_score(*args, **kwargs):
+    return GetUSRScore(*args, **kwargs)
+
+
+@rdkit_tool(enabled=False)
+def get_usr(*args, **kwargs):
+    return GetUSR(*args, **kwargs)
