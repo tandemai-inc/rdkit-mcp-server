17e0d809658a674d998473f4fcfa7c469df61285
Mike Rosengrant <michael.rosengrant@tandemai.com>
2025-06-30 16:28:11 -0400

    Update imports

--- rdkit_mcp/base_tools.py
+++ rdkit_mcp/base_tools.py
@@ -5,7 +5,7 @@ from mcp.server.fastmcp.exceptions import ToolError
 from pathlib import Path
 from rdkit import Chem
 
-from server.settings import get_app_settings
+from settings import get_app_settings
 from .decorators import rdkit_tool
 from .types import PickledMol, Smiles
 import pickle
